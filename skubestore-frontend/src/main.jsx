f1e4cb7c5621eb6d20843720e9f850310aa688d9
AlexFirilov <alexfirilov@gmail.com>
2024-12-17 15:16:10 +0200

    Added styles.css file for universal styling of pages

--- skubestore-frontend/src/main.jsx
+++ skubestore-frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import App from './App';
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
