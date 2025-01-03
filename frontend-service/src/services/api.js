c8db4e91e419d1b670bd2365555a8bd1aa18bfae
AlexFirilov <alexfirilov@gmail.com>
2025-01-03 10:31:59 +0200

    Changed api.js to use BASE_URL const instead of old URLs...

--- frontend-service/src/services/api.js
+++ frontend-service/src/services/api.js
@@ -6,12 +6,12 @@ const BASE_URL = 'https://api.skubestore.click';
 
 // User Service APIs
 export const registerUser = async (userData) => {
-  const response = await axios.post(`${VITE_USER_SERVICE_URL}/register`, userData);
+  const response = await axios.post(`${BASE_URL}/register`, userData);
   return response.data;
 };
 
 export const loginUser = async (credentials) => {
-  const response = await axios.post(`${VITE_USER_SERVICE_URL}/login`, credentials);
+  const response = await axios.post(`${BASE_URL}/login`, credentials);
   // Save user email to local storage for simplicity
   localStorage.setItem('userEmail', response.data.user.email);
   return response.data;
@@ -19,12 +19,12 @@ export const loginUser = async (credentials) => {
 
 // Product Service APIs
 export const getProducts = async () => {
-  const response = await axios.get(`${VITE_PRODUCT_SERVICE_URL}/products/view`);
+  const response = await axios.get(`${BASE_URL}/products/view`);
   return response.data;
 };
 
 // Order Service APIs
 export const placeOrder = async (orderData) => {
-  const response = await axios.post(`${VITE_ORDER_SERVICE_URL}/orders/place`, orderData);
+  const response = await axios.post(`${BASE_URL}/orders/place`, orderData);
   return response.data;
 };
